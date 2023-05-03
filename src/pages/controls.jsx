371d78739442f392d4abf60a2b1b5c7a2389088a
Sumit Kumar <isumtk@gmail.com>
2023-05-03 15:03:09 +0530

    Fixes

--- src/pages/controls.jsx
+++ src/pages/controls.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import Link from 'next/link';
 import { ProductCardContext } from '../provider';
 import { useTheme } from 'next-themes';
+import Head from 'next/head';
 
 const themes = [
   { name: 'Snow' },
@@ -24,62 +25,70 @@ export default function Controls() {
   console.log({ theme });
 
   return (
-    <div className={`flex h-full w-full flex-1 flex-col bg-inherit`}>
-      <header className="h-14 bg-indigo-500 px-4 text-neutral-50">
-        <nav className="container mx-auto flex h-full items-center justify-between">
-          <Link
-            href={'/products'}
-            className="flex h-full items-center font-sans text-2xl font-medium"
-          >
-            Products
-          </Link>
-          <div className="flex items-center justify-center px-3">
-            <p className="font-sans text-base font-medium">Settings</p>
+    <>
+      <Head>
+        <title>Settings</title>
+      </Head>
+      <div className={`flex h-full w-full flex-1 flex-col bg-inherit`}>
+        <header className="h-14 bg-indigo-500 px-4 text-neutral-50">
+          <nav className="container mx-auto flex h-full items-center justify-between">
+            <Link
+              href={'/products'}
+              className="flex h-full items-center font-sans text-2xl font-medium"
+            >
+              Products
+            </Link>
+            <div className="flex items-center justify-center px-3">
+              <p className="font-sans text-base font-medium">Settings</p>
+            </div>
+          </nav>
+        </header>
+        <main className="h-full w-full flex-1 bg-inherit px-4">
+          <div className="container mx-auto flex flex-col pt-20 text-[#101010]">
+            <h2 className="text-2xl font-semibold">Settings</h2>
+            <section className="flex flex-col pt-10">
+              <div className="mb-3 text-lg">Background</div>
+              <select
+                className="rounded bg-blue-500 p-4 text-white"
+                onChange={(e) => setTheme(e.currentTarget.value)}
+                value={theme}
+              >
+                <option value="system">System</option>
+                {themes.map((t) => (
+                  <option
+                    key={t.name.toLowerCase()}
+                    value={t.name.toLowerCase()}
+                  >
+                    {t.name}
+                  </option>
+                ))}
+              </select>
+            </section>
+            <section className="flex flex-col pt-10">
+              <div className="mb-3 text-lg">Scrolling</div>
+              <select
+                className="rounded bg-blue-500 p-4 text-white"
+                onChange={(e) => handleScrollTypeChange(e.target.value)}
+                value={productCard.scrollType}
+              >
+                <option value={'horizontal'}>Horizontal</option>
+                <option value={'vertical'}>Vertical</option>
+              </select>
+            </section>
+            <section className="flex flex-col pt-10">
+              <div className="mb-3 text-lg">Card Type</div>
+              <select
+                className="rounded bg-blue-500 p-4 text-white"
+                onChange={(e) => handleCardTypeChange(e.target.value)}
+                value={productCard.type}
+              >
+                <option value={'cardA'}>Card A</option>
+                <option value={'cardB'}>Card B</option>
+              </select>
+            </section>
           </div>
-        </nav>
-      </header>
-      <main className="h-full w-full flex-1 bg-inherit px-4">
-        <div className="container mx-auto flex flex-col pt-20 text-[#101010]">
-          <h2 className="text-2xl font-semibold">Settings</h2>
-          <section className="flex flex-col pt-10">
-            <div className="mb-3 text-lg">Background</div>
-            <select
-              className="rounded bg-blue-500 p-4 text-white"
-              onChange={(e) => setTheme(e.currentTarget.value)}
-              value={theme}
-            >
-              <option value="system">System</option>
-              {themes.map((t) => (
-                <option key={t.name.toLowerCase()} value={t.name.toLowerCase()}>
-                  {t.name}
-                </option>
-              ))}
-            </select>
-          </section>
-          <section className="flex flex-col pt-10">
-            <div className="mb-3 text-lg">Scrolling</div>
-            <select
-              className="rounded bg-blue-500 p-4 text-white"
-              onChange={(e) => handleScrollTypeChange(e.target.value)}
-              value={productCard.scrollType}
-            >
-              <option value={'horizontal'}>Horizontal</option>
-              <option value={'vertical'}>Vertical</option>
-            </select>
-          </section>
-          <section className="flex flex-col pt-10">
-            <div className="mb-3 text-lg">Card Type</div>
-            <select
-              className="rounded bg-blue-500 p-4 text-white"
-              onChange={(e) => handleCardTypeChange(e.target.value)}
-              value={productCard.type}
-            >
-              <option value={'cardA'}>Card A</option>
-              <option value={'cardB'}>Card B</option>
-            </select>
-          </section>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -4,32 +4,38 @@ import Link from 'next/link';
 import { HorizontalCarousel } from '@/components/horizontal-carousel';
 import { VerticalCarousel } from '@/components/vertical-carousel';
 import { ProductCardContext } from '../provider';
+import Head from 'next/head';
 
 export default function Home({ data }) {
   const cardStyle = useContext(ProductCardContext);
   return (
-    <div className="flex h-full w-full flex-col bg-neutral-50 pb-4">
-      <header className="mb-4 h-14 bg-indigo-500 px-4 text-neutral-50">
-        <nav className="container mx-auto flex h-full items-center justify-between">
-          <div className="flex h-full items-center font-sans text-2xl font-medium">
-            Products
+    <>
+      <Head>
+        <title>Products</title>
+      </Head>
+      <div className="flex h-full w-full flex-col bg-neutral-50 pb-4">
+        <header className="mb-4 h-14 bg-indigo-500 px-4 text-neutral-50">
+          <nav className="container mx-auto flex h-full items-center justify-between">
+            <div className="flex h-full items-center font-sans text-2xl font-medium">
+              Products
+            </div>
+            <Link
+              href={'/controls'}
+              className="flex items-center justify-center px-3"
+            >
+              <p className="font-sans text-base font-medium">Settings</p>
+            </Link>
+          </nav>
+        </header>
+        {cardStyle.productCard.scrollType === 'vertical' ? (
+          <VerticalCarousel data={data} />
+        ) : (
+          <div className="flex h-full w-full flex-1 items-center justify-center overflow-auto">
+            <HorizontalCarousel data={data} />
           </div>
-          <Link
-            href={'/controls'}
-            className="flex items-center justify-center px-3"
-          >
-            <p className="font-sans text-base font-medium">Settings</p>
-          </Link>
-        </nav>
-      </header>
-      {cardStyle.productCard.scrollType === 'vertical' ? (
-        <VerticalCarousel data={data} />
-      ) : (
-        <div className="flex h-full w-full flex-1 items-center justify-center overflow-auto">
-          <HorizontalCarousel data={data} />
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
 
