2c7344eaa8553a0277ed60ec9eee82ae333cbdb7
Sumit Kumar <isumtk@gmail.com>
2023-05-03 14:32:09 +0530

    Added Star Rating

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { HorizontalCarousel } from '@/components/horizontal-carousel';
